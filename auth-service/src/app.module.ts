825d09b6c42c4744a6dc98290737e668beadc0ba
Alvian Ghifari <alvian2002286@gmail.com>
2022-12-25 23:33:29 +0700

    auth_svc: setup db and main.ts

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'micro_auth',
+      username: 'root',
+      password: 'root',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
